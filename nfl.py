c34205edab50c7b789e1ab719d2b90a479824ff4
fearlessfreap24 <dylanaperez@yahoo.com>
2019-12-22 16:12:44 -0600

    added capitalization to team names

--- nfl.py
+++ nfl.py
@@ -9,7 +9,8 @@ gms = tree[0]
 
 for g in gms:
 #     print(g.tag)
-    print("Home: {:<20} Away: {:<20} Score: {} - {}".format(g.attrib['hnn'], g.attrib['vnn'], g.attrib['hs'], g.attrib['vs']), end="")
+    print("Home: {:<20} Away: {:<20} Score: {} - {}".format(g.attrib['hnn'].capitalize(),
+        g.attrib['vnn'].capitalize(), g.attrib['hs'], g.attrib['vs']), end="")
     if g.attrib['q'] == "F":
         print(" -- Final")
     else:
