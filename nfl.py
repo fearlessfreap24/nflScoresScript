5567dfeb2e9fa0a9775fcbeea97810766b8991a1
fearlessfreap24 <dylanaperez@yahoo.com>
2020-01-04 20:20:59 -0600

    turned into Flask app

--- nfl.py
+++ nfl.py
@@ -1,5 +1,8 @@
+from flask import Flask
 import xml.etree.ElementTree as ET
 import requests
+
+app = Flask(__name__)
 
 x = requests.get("http://www.nfl.com/liveupdate/scorestrip/ss.xml")
 
@@ -7,12 +10,26 @@ tree = ET.fromstring(x.text)
 
 gms = tree[0]
 
+ret = '''<table><tr><th>Home Team</th><th>Away Team</th><th>Score</th><th>Quarter</th></tr>'''
+
 for g in gms:
 #     print(g.tag)
-    print("Home: {:<20} Away: {:<20} Score: {:<2} - {:<2}".format(g.attrib['hnn'].capitalize(),
-        g.attrib['vnn'].capitalize(), g.attrib['hs'], g.attrib['vs']), end="")
+    ret = ret + "<tr><td>Home: {:<20}</td><td>Away: {:<20}</td><td>Score: {:<2} - {:<2}</td><td>".format(g.attrib['hnn'].capitalize(),
+        g.attrib['vnn'].capitalize(), g.attrib['hs'], g.attrib['vs'])
     if g.attrib['q'] == "F":
-        print(" -- Final")
+        ret = ret + " -- Final</td></tr>"
+    elif g.attrib['q'] == "FO":
+        ret = ret + " -- Final - Overtime</td></tr>"
+    elif g.attrib['q'] == 'H':
+        ret = ret + " -- Haftime</td></tr>"
+    elif g.attrib['q'] == '1' or g.attrib['q'] == '2' or g.attrib['q'] == '3' or g.attrib['q'] == '4':
+        ret = ret + " quarter: {} - {}</td></tr>".format(g.attrib['q'], g.attrib['k'])
     else:
-        print(" quarter: {}".format(g.attrib['q']))
+        ret = ret + " quarter: {}</td></tr>".format(g.attrib['q'])
+
+ret = ret + "</table>"
+
+@app.route('/nfl')
+def nfl():
+    return ret
 
